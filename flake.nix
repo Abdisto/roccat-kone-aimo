009c421094e91ae6dab320400be06e7fb32290f8
Abdisto <nasiefabdullah@gmail.com>
2026-04-03 22:31:59 +0200

    also done via ai because why not

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "roccat kone aimo virtual device";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        pname = "roccat-kone-aimo-virtual";
+        version = "1.0.0";
+
+        src = ./src;
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          cmake
+          meson
+        ];
+
+        buildInputs = with pkgs; [
+          hidapi
+        ];
+
+        # Let cmake do its thing
+        cmakeFlags = [
+          "-DCMAKE_BUILD_TYPE=Release"
+        ];
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp roccat_virtual $out/bin/
+        '';
+      };
+
+      apps.${system}.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/roccat_virtual";
+      };
+    };
+}
