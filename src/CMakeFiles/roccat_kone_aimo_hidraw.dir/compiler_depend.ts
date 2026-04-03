e26021cd20392bfbab9822346420daf587e5a785
Abdisto <nasiefabdullah@gmail.com>
2026-04-03 22:14:28 +0200

    adding hidapi

--- src/CMakeFiles/roccat_kone_aimo_hidraw.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for roccat_kone_aimo_hidraw.
--- src/CMakeFiles/roccat_kone_aimo_hidraw.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for roccat_kone_aimo_hidraw.
-# This may be replaced when dependencies are built.
